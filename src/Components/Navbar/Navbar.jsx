37bfb3d7304c0bce8ce2dae38cc224e8172b9817
Md Hridoy Sheikh <mdhridoy3240@gmail.com>
2024-12-02 13:06:22 +0600

    about complete navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,42 @@
+import Link from 'next/link';
 import React from 'react';
+import Menu from '../Menu/Menu';
+import SearchBar from '../SearchBar/SearchBar';
+import NavIcons from '../NavIcons/NavIcons';
+import Image from 'next/image';
 
 const Navbar = () => {
     return (
-        <div>
-            <h1>This is Navbar</h1>
+        <div className='h-20 bg-blue-300 px-4 md:px-8 lg:px-16 xl:32 2xl:px-64 relative'>
+            {/* mobile */}
+            <div className='h-full flex items-center justify-between md:hidden'>
+                <Link href="/">
+                    <div className='text-2xl tracing-wide '>
+                        TERA SHOP
+                    </div>
+                </Link>
+                <Menu></Menu>
+            </div>
+            {/* Bigger screan */}
+            <div className='hidden md:flex items-center h-full gap-8'>
+                {/* Left  */}
+                <div className='w-1/3'>
+                    <Link href="/">
+
+                        <div className='text-2xl tracing-wide flex items-center gap-3'>
+                            <Image src="/logo.png" alt="" width={24} height={24} />
+                            TERA SHOP
+                        </div>
+                    </Link>
+
+                </div>
+                {/* Right  */}
+                <div className='w-2/3 flex items-center justify-between gap-8'>
+                    <SearchBar></SearchBar>
+                    <NavIcons></NavIcons>
+                </div>
+
+            </div>
         </div>
     );
 };
